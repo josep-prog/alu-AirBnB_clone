4efd92df61efebad18abd7fb6b282d176b9df8a0
Joseph Nishimwe <j.nishimwe@alustudent.com>
2025-05-11 13:06:53 +0200

    __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,6 @@
+# models/__init__.py
 from models.engine.file_storage import FileStorage
+
+# This needs to be after the import to avoid circular dependency
 storage = FileStorage()
 storage.reload()
